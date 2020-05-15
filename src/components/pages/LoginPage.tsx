c0f16a16fd4e04d986934419accdd190da343d51
--help <domsbuhendwa2@gmail.com>
2020-05-15 06:09:20 +0200

    copied code test

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -33,12 +33,8 @@ const LoginPage: React.FC = React.memo(() => {
       <Spin spinning={isLoading}>
         <Content>
           <Layout>
-            <Sider className="site-layout-background" style={{ width: 400 }}>
-              <img
-                src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&w=1000&q=80"
-                alt=""
-                style={{ height: "100%" }}
-              />
+            <Sider className="site-layout-background" style={{ minWidth: 600 }}>
+              
             </Sider>
             <Content>
               <Card
