65987837747190c850db58dc288b67441830ad88
--help <domsbuhendwa2@gmail.com>
2020-05-15 06:19:04 +0200

    refractor homepage after remove antd

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,19 +1,7 @@
 import * as React from "react";
-import TopNavigation from "../views/TopNavigation";
-import FooterPage from "./FooterPage";
-import { Layout } from "antd";
 
 const HomePage: React.FC = () => {
-  const { Header, Content } = Layout;
-  return (
-    <Layout>
-      <Header>
-        <TopNavigation />
-      </Header>
-      <Content></Content>
-      <FooterPage />
-    </Layout>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
