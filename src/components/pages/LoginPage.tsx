86a2ac9e5509f612f4a8a5925287f88ee05eabc8
--help <domsbuhendwa2@gmail.com>
2020-05-15 06:35:56 +0200

    remove antd, refractore statetype

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
-import { Card, Layout, Spin } from "antd";
 import LoginForm from "../forms/LoginForm";
-import { UserState } from "../../store/types";
+import { StateType } from "../../store/types";
 import { useSelector, useDispatch } from "react-redux";
 import { logUserIn } from "../../store/actions";
 
-interface StateType {
-  user: UserState;
-}
-
 const LoginPage: React.FC = React.memo(() => {
-  const { Content, Sider } = Layout;
   const { isLoading } = useSelector((state: StateType) => state.user);
   const dispacth = useDispatch<any>();
 
@@ -23,33 +17,7 @@ const LoginPage: React.FC = React.memo(() => {
     );
   };
 
-  return (
-    <Layout
-      style={{
-        background: "#ecece",
-        padding: "30px",
-      }}
-    >
-      <Spin spinning={isLoading}>
-        <Content>
-          <Layout>
-            <Sider className="site-layout-background" style={{ minWidth: 600 }}>
-              
-            </Sider>
-            <Content>
-              <Card
-                title="Sign in to your account"
-                bordered={true}
-                style={{ width: 300 }}
-              >
-                <LoginForm onFinish={onFinish} />
-              </Card>
-            </Content>
-          </Layout>
-        </Content>
-      </Spin>
-    </Layout>
-  );
+  return <div></div>;
 });
 
 export default LoginPage;
