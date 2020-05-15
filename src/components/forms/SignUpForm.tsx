9aa2cfb694a970a45563cc2f4cb1f77f9f2eeeaf
--help <domsbuhendwa2@gmail.com>
2020-05-15 06:39:16 +0200

    remove antd

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -1,38 +1,7 @@
 import * as React from "react";
-import { Form, Input } from "antd";
-import "../pages/style.css";
 
 const SignUpForm: React.FC = () => {
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 2 },
-      sm: { span: 2 },
-    },
-    wrapperCol: {
-      xs: { span: 2 },
-      sm: { span: 4 },
-    },
-  };
-  return (
-    <Form {...formItemLayout}>
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-    </Form>
-  );
+  return <div></div>;
 };
 
 export default SignUpForm;
