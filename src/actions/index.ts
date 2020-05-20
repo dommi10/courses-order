b27a3533f2b998711bacc436340d4e6ecc617b37
--help <domsbuhendwa2@gmail.com>
2020-05-20 12:49:19 +0200

    setup ressource of fetch

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,43 @@
 import axios from "axios";
 import { User, Options } from "../store/types";
 
+export const fetchData = (options: Options) => {
+  const coursesPromise = fetchCourses(options);
+  return {
+    user: wrapPromise(coursesPromise),
+  };
+};
+
+const wrapPromise = (promise: Promise<any>) => {
+  //Set Initial Status
+  let status = "pending";
+  //Store result
+  let result: any;
+  //Wait for promise
+  let suspender = promise.then(
+    (res) => {
+      status = "success";
+      result = res;
+    },
+    (err) => {
+      status = "error";
+      result = err;
+    }
+  );
+
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      } else if (status === "error") {
+        throw result;
+      } else {
+        return result;
+      }
+    },
+  };
+};
+
 export const sigin = (user: User): Promise<User> => {
   return axios.post("/api/auth/sign", user).then((response) => response.data);
 };
@@ -11,7 +48,8 @@ export const signup = (user: User): Promise<User> => {
     .then((response) => response.data);
 };
 
-export const courses = (option: Options) => {
+//fetch courses
+export const fetchCourses = (option: Options) => {
   return axios
     .get("/api/courses", {
       data: option,
