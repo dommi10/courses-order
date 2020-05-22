a6d63decc268b60afb4498eb28761fa1adbb3f2e
--help <domsbuhendwa2@gmail.com>
2020-05-22 10:56:34 +0200

    setup card page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
+import CartPage from "./components/pages/CartPage";
 import PrivateRoute from "./components/routes/PrivateRoute";
 import PublicRoute from "./components/routes/PublicRoute";
 import LoginPage from "./components/pages/LoginPage";
@@ -13,6 +14,7 @@ function App() {
       <PublicRoute exact path="/login" component={LoginPage} />
       <PublicRoute exact path="/signup" component={SignUpPage} />
       <PrivateRoute exact path="/" component={HomePage} />
+      <PrivateRoute exact path="/cart" component={CartPage} />
       <FooterPage />
     </Router>
   );
---
+++ src/components/pages/CartPage.tsx
@@ -0,0 +1,101 @@
+import * as React from "react";
+import {
+  Table,
+  Grid,
+  Container,
+  Header,
+  Button,
+  Icon,
+} from "semantic-ui-react";
+import MenuPage from "../views/MenuPage";
+import { useSelector } from "react-redux";
+import { StateType } from "../../store/types";
+
+interface Props {}
+
+const CartPage: React.FC<Props> = React.memo(() => {
+  const { data } = useSelector((state: StateType) => state.card);
+
+  return (
+    <div>
+      <MenuPage />
+      <Container style={{ padding: "3%  2% " }}>
+        <Grid>
+          <Grid.Row>
+            <Header as="h1">Checkout</Header>
+          </Grid.Row>
+          <Grid.Row>
+            {data?.length === 0 ? (
+              <Container>
+                <Grid>
+                  <Grid.Column width="6" />
+                  <Grid.Column width="10">
+                    <Header as="h3" color="orange">
+                      {" "}
+                      <Icon name="warning circle" />
+                      Nothing to Show
+                    </Header>
+                  </Grid.Column>
+                </Grid>
+              </Container>
+            ) : (
+              <Table size="small" unstackable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Product</Table.HeaderCell>
+                    <Table.HeaderCell>Quantity</Table.HeaderCell>
+                    <Table.HeaderCell>Price</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  {data?.map((course) => (
+                    <Table.Row key={course.id}>
+                      <Table.Cell>{course.title}</Table.Cell>
+                      <Table.Cell>1</Table.Cell>
+                      <Table.Cell>{course.prix}</Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+
+                <Table.Footer fullWidth>
+                  <Table.Row>
+                    <Table.HeaderCell>
+                      <Header as="h4" color="red">
+                        {data?.length} Products
+                      </Header>
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                      <Header as="h4" color="green">
+                        Total {data?.length}
+                      </Header>
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                      <Header as="h4" color="green">
+                        USD{" "}
+                        {data
+                          ?.reduce((prev, course) => prev + course.prix, 0)
+                          .toPrecision(3)}
+                      </Header>
+                    </Table.HeaderCell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.HeaderCell />
+                    <Table.HeaderCell />
+                    <Table.HeaderCell>
+                      <Button primary>
+                        <Icon name="credit card" />
+                        Pay
+                      </Button>
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Footer>
+              </Table>
+            )}
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </div>
+  );
+});
+export default CartPage;
